2dfeb3d21703f7dbcdae778660d26f64a4e89894
theresac23 <theresachen@berkeley.edu>
2020-12-06 21:36:58 -0800

    fixed some syntax, not giving valid solutions

--- solver.py
+++ solver.py
@@ -20,7 +20,7 @@ def solve(G, s):
     rooms_s = {} # room : stress level
     k = len(G)
     hap = 0
-    for i in len(G):
+    for i in range(len(G)):
         st[i] = i
         rooms[i] = [i]
         rooms_s[i] = 0
@@ -29,75 +29,76 @@ def solve(G, s):
         s1, s2, e = edge # output looks like (0, 9, {'happiness': 7.0, 'stress': 9.514})
         r1 = st[s1] # s1's room
         r2 = st[s2] # s2's room
-        if len(rooms[r1]) == 1: # if student 1 is by themself then add to s2's room
+        if len(rooms[st[r1]]) == 1: # if student 1 is by themself then add to s2's room
             if room_stress(G, st, rooms, s1, s2) + rooms_s[s2] <= s/(k-1): # making sure new stress added to existing stress < thrshld
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
                 rooms[r1].remove(s1) # remove s1 from its own room
                 st[s1] = r2 # set s1's room to s2's room
-                rooms[r2].add(s1) # add s1 to s2's room
+                rooms[r2].append(s1) # add s1 to s2's room
                 k -= 1 
-                hap = hap + incr_hap(G, st, rooms, s1, s2)
-        else if len(rooms[students[s2]]) == 1: # add s2 to s1's room
+        elif len(rooms[st[s2]]) == 1: # add s2 to s1's room
             if room_stress(G, st, rooms, s1, s2) + rooms_s[s1] <= s/(k-1):
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
                 rooms[r2].remove(s2) # remove s1 from its own room
                 st[s2] = r1 # set s1's room to s2's room
-                rooms[r1].add(s2) # add s1 to s2's room
+                rooms[r1].append(s2) # add s1 to s2's room
                 k -= 1 
-                hap = hap + incr_hap(G, st, rooms, s1, s2)
-        else if r1 != s2_r: # in different rooms both > 1, merge s1 into s2's room
+        elif r1 != r2: # in different rooms both > 1, merge s1 into s2's room
             if room_stress(G, st, rooms, s1, s2) + rooms_s[s1] + rooms_s[s2] <= s/(k-1):
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
                 for stu in rooms[s1_r]: # move all of s1's room's students in s2's room
-                    rooms[r2].add(stu)
+                    rooms[r2].append(stu)
                     st[stu] = r2
                 rooms[r1].clear() 
                 k -= 1
-                hap = hap + incr_hap(G, st, rooms, s1, s2)
         else: # already in same room
             continue 
     k = 0
     for r in rooms:
-        if len(r) > 0:
+        if len(rooms[r]) > 0:
             k += 1
 
-    return D, k
+    return st, k
 
+def incr_hap(G, st, rooms, s1, s2):
+    """
+    Calculates happiness between s1's room's students and s2's room's students.
+    """ 
+    hapenis = 0
+    r1 = rooms[st[s1]]
+    r2 = rooms[st[s2]]
+    for st1 in r1:
+        for st2 in r2:
+            hapenis += G.get_edge_data(st1, st2)['happiness']
+    return hapenis
 
 def room_stress(G, st, rooms, s1, s2):
     """
     Calculates stress between s1's room's students and s2's room's students.
     """    
     stress = 0
-    r1_st = rooms[st[s1]]
-    r2_st = rooms[st[s2]]
+    r1 = rooms[st[s1]]
+    r2 = rooms[st[s2]]
     for st1 in r1:
         for st2 in r2:
             stress += G.get_edge_data(st1, st2)['stress']
     return stress
             
     
-def incr_hap(G, st, rooms, s1, s2):
-    """
-    Calculates happiness between s1's room's students and s2's room's students.
-    """ 
-    hapenis = 0
-    r1_st = rooms[st[s1]]
-    r2_st = rooms[st[s2]]
-    for st1 in r1:
-        for st2 in r2:
-            hapenis += G.get_edge_data(st1, st2)['happiness']
-    return hapenis
+
 
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G, s = read_input_file(path)
-#     D, k = solve(G, s)
-#     assert is_valid_solution(D, G, s, k)
-#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'outputs/small-1.out')
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    D, k = solve(G, s)
+    assert is_valid_solution(D, G, s, k)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    write_output_file(D, 'outputs/small-1.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
