e2cce0d14b0efa8ba8b8da58a6d11b6b61b867ba
theresac23 <theresachen@berkeley.edu>
2020-12-06 20:40:52 -0800

    first commit

--- solver.py
+++ solver.py
@@ -15,9 +15,77 @@ def solve(G, s):
         D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
         k: Number of breakout rooms
     """
-    pass
+    st = {} # student : assigned room
+    rooms = {} # room : [students]
+    rooms_s = {} # room : stress level
+    k = len(G)
+    hap = 0
+    for i in len(G):
+        st[i] = i
+        rooms[i] = [i]
+        rooms_s[i] = 0
+    sorted = sorted(G.edges(data=True),key= lambda x: x[2]['happiness'], reverse=True)
+    for edge in sorted: #change
+        s1, s2, e = edge # output looks like (0, 9, {'happiness': 7.0, 'stress': 9.514})
+        r1 = st[s1] # s1's room
+        r2 = st[s2] # s2's room
+        if len(rooms[r1]) == 1: # if student 1 is by themself then add to s2's room
+            if room_stress(G, st, rooms, s1, s2) + rooms_s[s2] <= s/(k-1): # making sure new stress added to existing stress < thrshld
+                rooms[r1].remove(s1) # remove s1 from its own room
+                st[s1] = r2 # set s1's room to s2's room
+                rooms[r2].add(s1) # add s1 to s2's room
+                k -= 1 
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
+        else if len(rooms[students[s2]]) == 1: # add s2 to s1's room
+            if room_stress(G, st, rooms, s1, s2) + rooms_s[s1] <= s/(k-1):
+                rooms[r2].remove(s2) # remove s1 from its own room
+                st[s2] = r1 # set s1's room to s2's room
+                rooms[r1].add(s2) # add s1 to s2's room
+                k -= 1 
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
+        else if r1 != s2_r: # in different rooms both > 1, merge s1 into s2's room
+            if room_stress(G, st, rooms, s1, s2) + rooms_s[s1] + rooms_s[s2] <= s/(k-1):
+                for stu in rooms[s1_r]: # move all of s1's room's students in s2's room
+                    rooms[r2].add(stu)
+                    st[stu] = r2
+                rooms[r1].clear() 
+                k -= 1
+                hap = hap + incr_hap(G, st, rooms, s1, s2)
+        else: # already in same room
+            continue 
+    k = 0
+    for r in rooms:
+        if len(r) > 0:
+            k += 1
+
+    return D, k
 
 
+def room_stress(G, st, rooms, s1, s2):
+    """
+    Calculates stress between s1's room's students and s2's room's students.
+    """    
+    stress = 0
+    r1_st = rooms[st[s1]]
+    r2_st = rooms[st[s2]]
+    for st1 in r1:
+        for st2 in r2:
+            stress += G.get_edge_data(st1, st2)['stress']
+    return stress
+            
+    
+def incr_hap(G, st, rooms, s1, s2):
+    """
+    Calculates happiness between s1's room's students and s2's room's students.
+    """ 
+    hapenis = 0
+    r1_st = rooms[st[s1]]
+    r2_st = rooms[st[s2]]
+    for st1 in r1:
+        for st2 in r2:
+            hapenis += G.get_edge_data(st1, st2)['happiness']
+    return hapenis
+    
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
